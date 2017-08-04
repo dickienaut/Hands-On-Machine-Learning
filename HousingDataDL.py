6811f938b1b91d4580b9334136edc8391bf8d728
Lyle Dickie <lyle.dickie@tradelegs.com>
2017-08-04 13:11:07 -0400

    rewrote download function and updated gitignore

--- HousingDataDL.py
+++ HousingDataDL.py
@@ -3,14 +3,15 @@ import tarfile
 from six.moves import urllib
 import pandas as pd
 
-DOWNLOAD_ROOT = 'https://github.com/ageron/handson-ml/master'
-HOUSING_PATH = 'datasets/housing'
-HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + 'housing.tgz'
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
-    tgz_path = os.path.join(housing_path, 'housing.tgz')
+    tgz_path = os.path.join(housing_path, "housing.tgz")
     urllib.request.urlretrieve(housing_url, tgz_path)
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
@@ -19,3 +20,9 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, 'housing.csv')
     return pd.read_csv(csv_path)
+
+
+fetch_housing_data()
+
+df = load_housing_data()
+print(df.info())
