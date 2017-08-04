00309063717c22e721cb1f3224d2147a597db444
Lyle Dickie <lyle.dickie@tradelegs.com>
2017-08-04 11:56:30 -0400

    initial commit

---
+++ HousingDataDL.py
@@ -0,0 +1,16 @@
+import os
+import tarfile
+from six.moves import urllib
+
+DOWNLOAD_ROOT = 'https://github.com/ageron/handson-ml/master'
+HOUSING_PATH = 'datasets/housing'
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + 'housing.tgz'
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, 'housing.tgz')
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
